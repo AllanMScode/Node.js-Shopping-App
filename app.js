385b6cc74d79e2fbbcd5b3cd990a6b6a4d9242c5
AllanMScode <97829785+AllanMScode@users.noreply.github.com>
2024-05-14 11:17:10 +0530

    ejs

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 // Importing admin routes
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 // creating an express application & storing in a constant named app
 const app = express();
+
+// setting the global configuration value
+// app.set() allows us to set any values globally on our application
+
+// we use the 'view engine' property for spicifying the template engine
+app.set("view engine", "ejs");
+
+// we use the 'views' property to specify where to find the folder with ejs files(in this case, it's the views folder)
+app.set("views", "views");
 
 // the below function registers a middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,13 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // order matters (in the order of how they're imported)
-app.use("/admin", adminRoutes); //we can add this filter if all admin routes will start with /admin
+app.use("/admin", adminData.routes); //we can add this filter if all admin routes will start with /admin
 app.use(shopRoutes);
 
 // if none of the above routes are selected we add a default route for 404 page below
 app.use((req, res, next) => {
-  // res.status(404).send("<h1>Page not found. erroR 404</h1>");
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "PagE nOt foUNd" });
 });
 
 // use is a method which is defined by the express framework.
